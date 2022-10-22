195c174c3b228f534106fdf17383cb2be394e63a
Uddeshya <uddeshya_kumar@ymail.com>
2022-10-22 08:33:30 +0530

    tick audio added

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ const minuteHandDom = document.querySelector('.clock .minute');
 const hourHandDom = document.querySelector('.clock .hour');
 const digitalDom = document.querySelector('.clock .digital');
 
+const tickAudio = new Audio('/tick.mp3');
+
 const zeroPad = (num) => ('0' + num).slice(-2);
+let prevSec = null;
+let seconds = null;
 
 const renderClock = () => {
     const date = new Date();
-    const seconds = date.getSeconds();
+    prevSec = seconds;
+    seconds = date.getSeconds();
     const minutes = date.getMinutes();
     const hours24 = date.getHours();
     const hours12 = hours24 % 12 || 12;
@@ -16,6 +21,11 @@ const renderClock = () => {
     minuteHandDom.style.rotate = ((minutes * 60 + seconds) * 0.1) + 'deg';
     hourHandDom.style.rotate = ((hours12 * 3600 + minutes * 60 + seconds) / 120) + 'deg';
     digitalDom.innerText = `${zeroPad(hours12)}:${zeroPad(minutes)}:${zeroPad(seconds)} ${meridiem}`;
+
+    if(prevSec != seconds) {
+        tickAudio.play();
+    }
+
     requestAnimationFrame(renderClock);
 }
 
